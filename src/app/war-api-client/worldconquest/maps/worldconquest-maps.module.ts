425eafba84aeab1a89827236e81b259df971fdf0
sagitta-ignis <sagitta.ignis@gmail.com>
2018-05-03 15:43:19 +0200

    Remove import from missing file.

--- src/app/war-api-client/worldconquest/maps/worldconquest-maps.module.ts
+++ src/app/war-api-client/worldconquest/maps/worldconquest-maps.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { WorldconquestMapsHttpService } from './worldconquest-maps-http.service';
-import { WorldconquestMapIconImageService } from './worldconquest-map-icon-image.service';
 import { WarApiClientModule } from '../../war-api-client.module';
 
 @NgModule({
